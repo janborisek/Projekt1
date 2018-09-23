3cb50cd13c938da6a0f7dfc18887d0fcc91e2969
janborisek <42959870+janborisek@users.noreply.github.com>
2018-09-23 14:41:07 +0200

    lahko brises zaposlene

--- brisi_ocena.php
+++ brisi_ocena.php
@@ -6,14 +6,15 @@
 <head><link rel="stylesheet" type="text/css" href="prvo.css"></head>
 <h1>Brisanje ocene</h1>
 <form method="get">
-    <?php echo '<input type="hidden" name="id" value="'.$_GET['id'].'">' ?>
-    <p>Ste prepričani?</p>
+    <?php 
+    echo '<input type="hidden" name="id" value="'.$_GET['id'].'">' ;
+    $id=$_GET['id'];
+    ?>
+    <p>Ste prepričani? <br>Izbrisali boste oceno</p>
     <input type="submit" name="submit" value="Izbriši">
 </form>
-<?php
-    if(isset($_GET['id']){
-        $id = $_GET['id'];
 
-        
-    }
+<?php
+    $stmt = $pdo->prepare("DELETE FROM ocene WHERE id = ?");
+    $stmt->execute([$id]);
 ?>
--- brisi_zaposleni.php
+++ brisi_zaposleni.php
@@ -5,8 +5,16 @@
 ?>
 <head><link rel="stylesheet" type="text/css" href="prvo.css"></head>
 <h1>Brisanje zaposlenega</h1>
-<form action="brisi_preveri.php" method="get">
-    <?php echo '<input type="hidden" name="email" value="'.$_GET['email'].'">' ?>
-    <p>Ste prepričani?</p>
-    <input type="submit" name="submit" value="Izbriši">
+
+<form method="get">
+    <?php 
+    echo '<input type="hidden" name="email" value="'.$_GET['email'].'">' ;
+    $email=$_GET['email'];
+    ?>
 </form>
+
+<?php
+    $stmt = $pdo->prepare("DELETE FROM zaposleni WHERE email = ?");
+    $stmt->execute([$email]);
+    header("location: stran.php");
+?>
