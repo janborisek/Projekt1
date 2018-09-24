9dc32f90101b5ac8ac3162433de197c6ba2536d6
janborisek <42959870+janborisek@users.noreply.github.com>
2018-09-24 21:11:22 +0200

    google login

---
+++ google.php
@@ -0,0 +1,55 @@
+<?php 
+require_once "povezava.php";
+session_start();
+require ("vendor/autoload.php");
+//Step 1: Enter you google account credentials
+$g_client = new Google_Client();
+$g_client->setClientId("854240138875-d8va9bo7nib88oh5ful4t3h4ruo41g3s.apps.googleusercontent.com");
+$g_client->setClientSecret("lBp1NYQtl__q2z7znA7LWYl7");
+$g_client->setRedirectUri("http://localhost/ocene_zaposleni/google.php");
+$g_client->setScopes("email");
+//Step 2 : Create the url
+$auth_url = $g_client->createAuthUrl();
+echo "<a href='$auth_url'>Prijava preko Google računa </a>";
+//Step 3 : Get the authorization  code
+$code = isset($_GET['code']) ? $_GET['code'] : NULL;
+//Step 4: Get access token
+if(isset($code)) {
+    try {
+        $token = $g_client->fetchAccessTokenWithAuthCode($code);
+        $g_client->setAccessToken($token);
+    }catch (Exception $e){
+        echo $e->getMessage();
+    }
+    try {
+        $pay_load = $g_client->verifyIdToken();
+    }catch (Exception $e) {
+        echo $e->getMessage();
+    }
+} else{
+    $pay_load = null;
+}
+if(isset($pay_load)){ 
+    $email=$pay_load["email"];
+    
+    $stmt = $pdo->prepare('SELECT * FROM uporabniki WHERE email = ?');
+$stmt->execute([$email]);
+$user = $stmt->fetch();
+
+if($user)
+{   
+    //pošlje te na stran
+    $row=$user;
+    $_SESSION['id']=$row['ID'];
+    $_SESSION['email']=$row['email'];
+    header("location: stran.php");
+}
+ else {
+	//ce ni emaila te vpise v bazo
+	$sql = "INSERT INTO uporabniki (email) VALUES (?)";
+	$stmt= $pdo->prepare($sql);
+	$stmt->execute([$email]);
+    
+    header("location: prijava.php");
+ }
+}
